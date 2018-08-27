abd5136cd24d417078f3b3b9567afe8f99f5c10f
FireWaveX <alexledeme@otmail.fr>
2018-08-27 16:17:23 +0200

    header menu

---
+++ files/Login.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+</head>
+<body>
+
+<header>
+	<h1>Jzit Music</h1>
+	 <ul>
+	  <li><a href="../files/Homepage.php">Homepage</a></li>
+	  <li><a href="../files/Request.php">Request a song</a></li>
+	  <li><a href="../files/About.php">About</a></li>
+	  <li><a class="active" href="../files/Login.php">Login</a></li>
+	</ul> 
+</header>
+
+
+<script type='text/javascript' src='../js/jquery.js'></script>
+<script type='text/javascript' src='../js/jquery-ui.js'></script>
+<script type='text/javascript' src='../js/main.js'></script>
+</body>
+</html>
