4e0dff9f5f059f38f2ce78103484e10bfe63f6bd
FireWaveX <alexledeme@otmail.fr>
2018-08-27 16:10:18 +0200

    navbar

--- index.php
+++ index.php
@@ -8,10 +8,14 @@
 <body>
 
 <header>
-	<h1>Hello World !</h1>
+	<h1>Jzit Music</h1>
+	 <ul>
+	  <li><a class="active" href="files/Homepage.php">Homepage</a></li>
+	  <li><a href="files/Request.php">Request a song</a></li>
+	  <li><a href="files/About.php">About</a></li>
+	  <li><a href="files/Login.php">Login</a></li>
+	</ul> 
 </header>
-
-
 
 
 <script type='text/javascript' src='js/jquery.js'></script>
