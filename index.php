a320b6c59d8535ba4cc164ae22215111614567cc
FireWaveX <alexledeme@otmail.fr>
2018-08-27 15:55:22 +0200

    add project folder and files

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+
+<header>
+	<h1>Hello World !</h1>
+</header>
+
+
+
+
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/jquery-ui.js'></script>
+<script type='text/javascript' src='js/main.js'></script>
+</body>
+</html>
