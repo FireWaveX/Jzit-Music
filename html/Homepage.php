f5c8606a6a101a849aa000d471f48f5a8133de38
FireWaveX <alexledeme@otmail.fr>
2018-08-27 17:35:27 +0200

    form to request song

--- html/Homepage.php
+++ html/Homepage.php
@@ -9,10 +9,10 @@
 <header>
 	<h1>Jzit Music</h1>
 	 <ul>
-	  <li><a href="../files/Homepage.php">Homepage</a></li>
-	  <li><a href="../files/Request.php">Request a song</a></li>
-	  <li><a href="../files/About.php">About</a></li>
-	  <li><a class="active" href="../files/Login.php">Login</a></li>
+	  <li><a class="active" href="../html/Homepage.php">Homepage</a></li>
+	  <li><a href="../html/Request.php">Request a song</a></li>
+	  <li><a href="../html/About.php">About</a></li>
+	  <li><a href="../html/Login.php">Login</a></li>
 	</ul> 
 </header>
 
