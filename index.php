29c5dead1fb8f30023ce0c6135d725341de85664
Veylord74 <veylord74@hotmail.fr>
2018-08-29 09:59:20 +0200

    New singer image & display work

--- index.php
+++ index.php
@@ -1,20 +1,21 @@
 <!DOCTYPE html> 
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
-<title></title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Jzit Music</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 <body>
 
 <header>
-	<h1>Jzit Music</h1>
+	<h1 class="title">Jzit Music</h1>
 	 <ul>
-	  <li><a class="active menu Hom">Homepage</a></li>
-	  <li><a class="menu Req">Request a song</a></li>
-	  <li><a class="menu Abo">About</a></li>
-	  <li><a class="menu Log">Login</a></li>
+	  <li><a class="active menu Hom" href="#">Homepage</a></li>
+	  <li><a class="menu Req" href="#">Request a song</a></li>
+	  <li><a class="menu Abo" href="#">About</a></li>
+	  <li><a class="menu Log" href="#">Login</a></li>
 	</ul> 
 </header>
 
@@ -26,19 +27,44 @@
 		<!-- <button class="action" type="button">Send</button> --> <!-- non vue.js option -->
 	 	<!-- <button v-on:click="request_button2">Send</button> --> <!-- no need, automatic now -->
 	 	<h2>Song list</h2>
+	 	<button class="btn refresh" v-on:click="request_button2">Refresh</button>
 
-	 	<div id="listOfSongs">
-	 		
-	 	</div>
+	 	<div id="listOfSongs"></div>
 
 	 	<div id="sing-btn">
-	 		<button v-on:click="request_button2">Let's sing !</button>
+	 		<button data-toggle="modal" data-target="#videoModal">Let's sing !</button>
 	 	</div>
+
+	 	<div class="modal" id="videoModal">
+		    <div class="modal-dialog">
+		      <div class="modal-content">
+		      
+		        <!-- Modal Header -->
+		        <div class="modal-header">
+		          <h4 class="modal-title">Music video</h4>
+		          <button type="button" class="close" data-dismiss="modal">&times;</button>
+		        </div>
+		        
+		        <!-- Modal body -->
+		        <div class="modal-body">
+		          Video here
+		        </div>
+		        
+		        <!-- Modal footer -->
+		        <div class="modal-footer">
+		          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+		        </div>
+		        
+		      </div>
+		    </div>
+	  	</div>
 
 	</div>
 
 
 	<div id="Re" class="content Re" hidden> <!-- w3-include-html="html/Request.php" -->
+
+		<img class="singer-img" src="images/Singer.png">
 
 	 	<form>
 		  Youtube link of the song<br>
@@ -48,17 +74,18 @@
 		</form>
 
 		<button class="request_button" type="button">Request</button>
-		
 
 	</div>
 
 
 	<div class="content Ab" hidden>
+			<img class="singeuse-img" src="images/Singeuse.png">
+			
 			<p>About this website</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 			<br/><br/>
 			Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
-			<img class="singer-img" src="images/Singer.png">
+
 	</div>  <!-- w3-include-html="html/About.php" -->
 
 	<div id="Re" class="content Lo" hidden>
@@ -82,5 +109,6 @@
 <script type='text/javascript' src='js/jquery.js'></script>
 <script type='text/javascript' src='js/jquery-ui.js'></script>
 <script type='text/javascript' src='js/main.js'></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 </html>
