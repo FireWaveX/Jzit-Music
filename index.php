7955895c0e1eacc88fc58d7bc471fc958df33a4c
Veylord74 <veylord74@hotmail.fr>
2018-08-29 11:45:51 +0200

    Responsiveness

--- index.php
+++ index.php
@@ -5,17 +5,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>Jzit Music</title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="css/style.css" />
+<link rel="stylesheet" type="text/css" media="screen and (max-width: 992px)" href="css/smallscreen.css" />
 </head>
 <body>
 
 <header>
 	<h1 class="title">Jzit Music</h1>
-	 <ul>
-	  <li><a class="active menu Hom" href="#">Homepage</a></li>
-	  <li><a class="menu Req" href="#">Request a song</a></li>
-	  <li><a class="menu Abo" href="#">About</a></li>
-	  <li><a class="menu Log" href="#">Login</a></li>
+	<ul class="topnav" id="myTopnav">
+		<li><a class="active menu Hom" href="#">Homepage</a></li>
+		<li><a class="menu Req" href="#">Request a song</a></li>
+		<li><a class="menu Abo" href="#">About</a></li>
+		<li><a class="menu Log" href="#">Login</a></li>
+		<li class="icon"><a class="menu ico" href="#" onclick="myFunction()">
+			<i class="fa fa-bars"></i>
+		</a></li>
 	</ul> 
 </header>
 
@@ -64,7 +69,7 @@
 
 	<div id="Re" class="content Re" hidden> <!-- w3-include-html="html/Request.php" -->
 
-		<img class="singer-img" src="images/Singer.png">
+		<img class="singer-img img" src="images/Singer.png">
 
 	 	<form>
 		  Youtube link of the song<br>
@@ -79,7 +84,7 @@
 
 
 	<div class="content Ab" hidden>
-			<img class="singeuse-img" src="images/Singeuse.png">
+			<img class="singeuse-img img" src="images/Singeuse.png">
 			
 			<p>About this website</p>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
