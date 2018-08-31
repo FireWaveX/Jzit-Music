0edb2efa8333e93564c453e568a67dd6d79c4257
FireWaveX <alexledeme@otmail.fr>
2018-08-31 17:00:26 +0200

    don't know where I am know

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-
   //getRequests();
 
 	$( ".request_button" ).click(function() {
@@ -17,9 +16,9 @@ $(document).ready(function() {
   //   $(this).addClass( "active" );
   // });
 
-  //showHTML();
+  // showHTML();
 
-	includeHTML();
+	// includeHTML();
 
 });
 
@@ -55,33 +54,7 @@ function addRequest() {
 
 
 // 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
-    }
-  }
-}
+
 
 function getId(url) {
     var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
@@ -211,6 +184,7 @@ var getSongs = new Vue({
 
     },
     login(name, password){
+      var vm = this
 
       fetch('https://apex.oracle.com/pls/apex/anime_keeper/Jzit/getUser').then(function(response) {
 
@@ -228,12 +202,11 @@ var getSongs = new Vue({
 
           console.log("attemp login")
           if (md5Password === item.password) {
-
             if (name === item.username) {
 
-              this.user = name;
+              vm.user ='';
+              vm.user = ' - ' + name;
               console.log("login sucess")
-              console.log(this.user)
               return;
             }
           }
@@ -564,4 +537,33 @@ var MD5 = function (string) {
 //     $(".Lo").removeAttr("hidden");
 //   });
 
+// }
+
+
+// function includeHTML() {
+//   var z, i, elmnt, file, xhttp;
+//   /*loop through a collection of all HTML elements:*/
+//   z = document.getElementsByTagName("*");
+//   for (i = 0; i < z.length; i++) {
+//     elmnt = z[i];
+//     /*search for elements with a certain atrribute:*/
+//     file = elmnt.getAttribute("w3-include-html");
+//     if (file) {
+//       /*make an HTTP request using the attribute value as the file name:*/
+//       xhttp = new XMLHttpRequest();
+//       xhttp.onreadystatechange = function() {
+//         if (this.readyState == 4) {
+//           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+//           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+//           /*remove the attribute, and call this function once more:*/
+//           elmnt.removeAttribute("w3-include-html");
+//           includeHTML();
+//         }
+//       }
+//       xhttp.open("GET", file, true);
+//       xhttp.send();
+//       /*exit the function:*/
+//       return;
+//     }
+//   }
 // }
