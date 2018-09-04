4df5b744009ef2d15f79c2baeb14093adba3d9c5
Veylord74 <veylord74@hotmail.fr>
2018-09-04 22:42:53 +0200

    Filled About section text & resized modal video Iframe

--- public/js/main.js
+++ public/js/main.js
@@ -67,7 +67,7 @@ function getId(url) {
     }
 }
 
-function myFunction() {
+function beResponsive() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
